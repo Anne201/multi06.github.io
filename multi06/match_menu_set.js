48607f304879684e510d5f1f04fa65a99443e4bc
Anne201 <118590269+Anne201@users.noreply.github.com>
2024-11-11 20:13:45 +0800

    V4

--- multi06/match_menu_set.js
+++ multi06/match_menu_set.js
@@ -1,6 +1,6 @@
 menu_title='對對碰';
 menu_items = new Array(
   '2有幾個,match_set_1.js'
-, '記憶大考驗,match_set_memory.js'
-, '九九乘法,match_set_9x9.js'
-);
+, '5的幾倍,match_set_2.js'
+, '幾有幾個和幾的幾倍,match_set_3.js'
+);尸
